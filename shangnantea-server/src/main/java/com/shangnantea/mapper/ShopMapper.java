6ee88aba67029633b5a6f1033d8dc0f049ed5bde
Yme <yoil@example.com>
2026-01-25 05:28:34 +0800

    chore(order): autosave 2026-01-25 05:28:32 [branch: feature/order-module]

--- shangnantea-server/src/main/java/com/shangnantea/mapper/ShopMapper.java
+++ shangnantea-server/src/main/java/com/shangnantea/mapper/ShopMapper.java
@@ -6,5 +6,10 @@ import com.shangnantea.model.entity.shop.Shop;
  * 店铺Mapper接口
  */
 public interface ShopMapper extends BaseMapper<Shop, String> {
-    // 额外的特定方法可以在这里声明
+    /**
+     * 根据店主用户ID查询店铺
+     * @param ownerId 店主用户ID
+     * @return 店铺信息
+     */
+    Shop selectByOwnerId(String ownerId);
 } 
