f01e1bb22e3d97fb816b6bfb2a160ff11dd2c52f
Yme <yoil@example.com>
2026-01-25 11:30:20 +0800

    chore: autosave 2026-01-25 11:30:20

--- shangnantea-server/src/main/java/com/shangnantea/model/entity/user/UserFollow.java
+++ shangnantea-server/src/main/java/com/shangnantea/model/entity/user/UserFollow.java
@@ -30,6 +30,16 @@ public class UserFollow {
     private String followId;
     
     /**
+     * 关注目标名称(冗余字段，优化列表展示)
+     */
+    private String targetName;
+    
+    /**
+     * 关注目标头像(冗余字段，优化列表展示)
+     */
+    private String targetAvatar;
+    
+    /**
      * 创建时间
      */
     private Date createTime;
