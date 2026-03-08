f30675ac99a9248cdd672d0c27f1ada7b699a33e
Yme <yoil@example.com>
2026-03-09 02:57:19 +0800

    chore: autosave 2026-03-09 02:57:19

--- shangnantea-server/src/main/java/com/shangnantea/service/impl/UserServiceImpl.java
+++ shangnantea-server/src/main/java/com/shangnantea/service/impl/UserServiceImpl.java
@@ -627,13 +627,7 @@ public class UserServiceImpl implements UserService {
             boolean hasUpdate = false;
             
             if (userData.containsKey("nickname")) {
-                String nickname = (String) userData.get("nickname");
-                // 如果昵称为空或只包含空格，自动使用用户名作为昵称
-                if (nickname == null || nickname.trim().isEmpty()) {
-                    user.setNickname(user.getUsername());
-                } else {
-                    user.setNickname(nickname.trim());
-                }
+                user.setNickname((String) userData.get("nickname"));
                 hasUpdate = true;
             }
             
