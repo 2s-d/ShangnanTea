05295475c80c0d3b50e3e60998019c9ac879bc57
Yme <yoil@example.com>
2026-03-16 15:39:47 +0800

    chore: autosave 2026-03-16 15:39:46

--- shangnantea-server/src/main/java/com/shangnantea/service/impl/UserServiceImpl.java
+++ shangnantea-server/src/main/java/com/shangnantea/service/impl/UserServiceImpl.java
@@ -77,7 +77,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
-import java.math.BigDecimal;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
